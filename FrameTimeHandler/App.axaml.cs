43b268fde938c21270d1975aed834693f615e1ba
Aleksandr Kovalyov <bahx4pc@gmail.com>
2020-07-12 22:06:46 +0300

    Add project files.

---
+++ FrameTimeHandler/App.axaml.cs
@@ -0,0 +1,30 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+
+using FrameTimeHandler.ViewModels;
+using FrameTimeHandler.Views;
+
+namespace FrameTimeHandler
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}

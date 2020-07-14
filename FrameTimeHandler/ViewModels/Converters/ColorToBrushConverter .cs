2ca00b5971ec082f056f72e8f76766262aab2c39
Aleksandr Kovalyov <bahx4pc@gmail.com>
2020-07-14 19:07:33 +0300

    ColorPicker integration; end interface

---
+++ FrameTimeHandler/ViewModels/Converters/ColorToBrushConverter .cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+
+namespace FrameTimeHandler.ViewModels.Converters
+{
+    class ColorToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new SolidColorBrush((Color)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((SolidColorBrush) value).Color;
+        }
+    }
+}
--- FrameTimeHandler/ViewModels/MainWindowViewModel.cs
+++ FrameTimeHandler/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Reactive;
-
+using System.Reactive.Joins;
+using System.Reactive.Linq;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
-
+using Avalonia.Controls.Shapes;
+using Avalonia.Media;
 using ReactiveUI;
 using DynamicData;
 
@@ -37,14 +40,80 @@ namespace FrameTimeHandler.ViewModels
 
         public ReactiveCommand<Unit, Unit> SelectFileCommand { get; }
         public ReactiveCommand<Unit, Unit> UnselectFileCommand { get; }
+        public ReactiveCommand<Unit, Unit> SaveGraphsCommand { get; }
+        public ReactiveCommand<Unit, Unit> ChangeCustomColorCommand { get; }
 
-        public ObservableCollection<string> LogCreationPrograms { get; set; } = new ObservableCollection<string>() { "none" };
-        public string SelectedProgram { get; set; }
+        public ObservableCollection<string> LogCreationPrograms { get; set; } = new ObservableCollection<string>();
 
-        public bool IsFrametimingGraphNeeded { get; set; } = false;
-        public bool IsProbabilityDensityGraphNeeded { get; set; } = false;
-        public bool IsProbabilityDistributionGraphNeeded { get; set; } = false;
+        private string _selectedProgram;
+        public string SelectedProgram
+        {
+            get => _selectedProgram;
+            set
+            {
+                if (_selectedProgram != value)
+                {
+                    _selectedProgram = value;
+                    this.RaisePropertyChanged(nameof(SelectedProgram));
+                }
+            }
+        }
 
+        private bool _isFrametimingGraphNeeded;
+        public bool IsFrametimingGraphNeeded
+        {
+            get => _isFrametimingGraphNeeded;
+            set
+            {
+                if (_isFrametimingGraphNeeded != value)
+                {
+                    _isFrametimingGraphNeeded = value;
+                    this.RaisePropertyChanged(nameof(IsFrametimingGraphNeeded));
+                }
+            }
+        }
+
+        private bool _isProbabilityDensityGraphNeeded;
+        public bool IsProbabilityDensityGraphNeeded
+        {
+            get => _isProbabilityDensityGraphNeeded;
+            set
+            {
+                if (_isProbabilityDensityGraphNeeded != value)
+                {
+                    _isProbabilityDensityGraphNeeded = value;
+                    this.RaisePropertyChanged(nameof(IsProbabilityDensityGraphNeeded));
+                }
+            }
+        }
+
+        private bool _isProbabilityDistributionGraphNeeded;
+        public bool IsProbabilityDistributionGraphNeeded
+        {
+            get => _isProbabilityDistributionGraphNeeded;
+            set
+            {
+                if (_isProbabilityDistributionGraphNeeded != value)
+                {
+                    _isProbabilityDistributionGraphNeeded = value;
+                    this.RaisePropertyChanged(nameof(IsProbabilityDistributionGraphNeeded));
+                }
+            }
+        }
+
+        private bool _isChangingColor;
+        public bool IsChangingColor
+        {
+            get => _isChangingColor;
+            set
+            {
+                if (_isChangingColor != value)
+                {
+                    _isChangingColor = value;
+                    this.RaisePropertyChanged(nameof(IsChangingColor));
+                }
+            }
+        }
 
         private ProgramsThatReadOutput _programThatReadOutput;
         public ProgramsThatReadOutput ProgramThatReadOutput
@@ -56,6 +125,20 @@ namespace FrameTimeHandler.ViewModels
                 {
                     _programThatReadOutput = value;
                     this.RaisePropertyChanged(nameof(ProgramThatReadOutput));
+                }
+            }
+        }
+
+        private Color _customColor;
+        public Color CustomColor
+        {
+            get => _customColor;
+            set
+            {
+                if (!_customColor.Equals(value))
+                {
+                    _customColor = value;
+                    this.RaisePropertyChanged(nameof(CustomColor));
                 }
             }
         }
@@ -83,8 +166,31 @@ namespace FrameTimeHandler.ViewModels
             {
                 this.FilePath = "";
             });
+            SaveGraphsCommand = ReactiveCommand.Create(() =>
+            {
+
+
+            }, this.WhenAnyValue(
+                vm => vm.IsFileSelected, 
+                vm => vm.SelectedProgram, 
+                vm => vm.ProgramThatReadOutput,
+                vm => vm.IsFrametimingGraphNeeded,
+                vm => vm.IsProbabilityDensityGraphNeeded, 
+                vm => vm.IsProbabilityDistributionGraphNeeded,
+                (isFileSelected, selectedProgram, programThatReadOutput, isFrametimingGraphNeeded, isProbabilityDensityGraphNeeded, isProbabilityDistributionGraphNeeded) 
+                    => isFileSelected 
+                       && !string.IsNullOrEmpty(selectedProgram) 
+                       && programThatReadOutput != ProgramsThatReadOutput.None 
+                       && (isFrametimingGraphNeeded || isProbabilityDensityGraphNeeded ||
+                           isProbabilityDistributionGraphNeeded)));
+
+            ChangeCustomColorCommand = ReactiveCommand.Create(() =>
+            {
+                IsChangingColor = !IsChangingColor;
+            });
 
             LogCreationPrograms.AddRange(FTAnlzer.SupportedPrograms);
+            CustomColor = Color.FromArgb(255, 0, 0, 0);
         }
     }
 }
